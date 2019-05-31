fa6f142e48702dbd5269610113b4deda9541e3b6
Josh Young <18503267+joshayoung@users.noreply.github.com>
2019-05-30 20:12:56 -0500

    Add Faraday Middleware and enpoints test

--- lib/weather.rb
+++ lib/weather.rb
@@ -2,4 +2,19 @@ class Weather
   def temperature
     "90"
   end
+
+  def forecast(latitude:, longitude:)
+    endpoints(latitude, longitude)
+  end
+
+  private
+
+  def endpoints(lat, long)
+    connection = Faraday.new(url: "https://api.weather.gov") do |faraday|
+      faraday.adapter Faraday.default_adapter
+      faraday.response :json
+    end
+    response = connection.get("points/#{lat},#{long}")
+    response.body
+  end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,17 +1,29 @@
 require_relative "../lib/weather.rb"
-require "faraday"
 require "pry"
 require "pry-remote"
 require "pry-nav"
+require "faraday"
+require "faraday_middleware"
+require "support/api_data"
 
 RSpec.describe Weather do
   describe "#temperature" do
     it "returns the current temperature" do
       weather = Weather.new
 
-      weather.temperature
+      temperature = weather.temperature
 
-      expect(weather.temperature).to eq("90")
+      expect(temperature).to eq("90")
+    end
+  end
+
+  describe "#forecast" do
+    it "returns the format for the latitude and longitude passed" do
+      weather = Weather.new
+
+      forecast = weather.forecast(latitude: "40.7127", longitude: "-74.0059")
+
+      expect(forecast).to eq(forecast_data)
     end
   end
 end
