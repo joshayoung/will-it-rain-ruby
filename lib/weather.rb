8b00e749041f6fbcb9c4f960d960e2701bcba6e7
Josh Young <18503267+joshayoung@users.noreply.github.com>
2019-06-01 18:39:12 -0500

    Remove unnecessary methods and make some private

--- lib/weather.rb
+++ lib/weather.rb
@@ -11,25 +11,12 @@ class Weather
     Day.new(response.body["properties"]["periods"].first)
   end
 
-  def points(latitude:, longitude:)
-    endpoints(latitude, longitude)
-  end
+private
 
   def forecast_url(latitude:, longitude:)
-    points = points(latitude: latitude, longitude: longitude)
+    points = endpoints(latitude, longitude)
     points["properties"]["forecast"]
   end
-
-  def forecast(url)
-    connection = Faraday.new(url: url) do |faraday|
-      faraday.adapter Faraday.default_adapter
-      faraday.response :json
-    end
-    response = connection.get(url)
-    response.body
-  end
-
-private
 
   def endpoints(lat, long)
     url = "https://api.weather.gov"
@@ -37,8 +24,6 @@ private
       faraday.adapter Faraday.default_adapter
       faraday.response :json
     end
-    url = url + "/points/#{lat},#{long}"
-    response = connection.get(url)
-    response.body
+    connection.get(url + "/points/#{lat},#{long}").body
   end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -7,32 +7,12 @@ require "faraday_middleware"
 require "support/api_data"
 
 RSpec.describe Weather do
-  describe "#forecast" do
-    it "returns the format for the latitude and longitude passed" do
-      weather = Weather.new
-
-      points = weather.points(latitude: "32.7766", longitude: "-96.7969")
-
-      expect(points).to eq(forecast_data)
-    end
-  end
-
   describe "#get" do
     it "return an object with current forecast" do
       weather = Weather.new
       forecast = weather.get("32.7766", "-96.7969")
 
       expect(forecast).to be_instance_of(Day)
-    end
-  end
-
-  describe "#forecast" do
-    it "returns the forecast URL" do
-      weather = Weather.new
-
-      forecast_url = weather.forecast_url(latitude: "32.7766", longitude: "-96.7969")
-
-      expect(forecast_url).to eq("https://api.weather.gov/gridpoints/FWD/88,103/forecast")
     end
   end
 end
