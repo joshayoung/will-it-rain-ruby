21beb487538d459f9f535065ab7f731feab8c3b6
Josh Young <j22cal@gmail.com>
2019-06-01 19:44:42 -0500

    Cleanup faraday connections

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,29 +1,19 @@
+require "fetch.rb"
 require "day.rb"
 
 class Weather
   def get(lat, long)
-    url = forecast_url(latitude: lat, longitude: long)
-    connection = Faraday.new(url: url) do |faraday|
-      faraday.adapter Faraday.default_adapter
-      faraday.response :json
-    end
-    response = connection.get(url)
-    Day.new(response.body["properties"]["periods"].first)
+    data = Fetch.retrieve(forecast_url(latitude: lat, longitude: long))
+    Day.new(data["properties"]["periods"].first)
   end
 
 private
 
   def forecast_url(latitude:, longitude:)
-    points = endpoints(latitude, longitude)
-    points["properties"]["forecast"]
+    endpoints(latitude, longitude)["properties"]["forecast"]
   end
 
   def endpoints(lat, long)
-    url = "https://api.weather.gov"
-    connection = Faraday.new(url: url) do |faraday|
-      faraday.adapter Faraday.default_adapter
-      faraday.response :json
-    end
-    connection.get(url + "/points/#{lat},#{long}").body
+    Fetch.retrieve("https://api.weather.gov/points/#{lat},#{long}")
   end
 end
