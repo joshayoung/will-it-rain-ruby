af35324b6cab7bfc558be1848c49fea17a8a671d
Josh Young <j22cal@gmail.com>
2019-06-15 11:21:13 -0500

    Add more test coverage

--- spec/mail_spec.rb
+++ spec/mail_spec.rb
@@ -20,5 +20,17 @@ RSpec.describe Mail do
 
       expect(email_message).to eq("    From: <\"from@example.com\">\n    To: <\"to@example.com\">\n    MIME-Version: 1.0\n    Content-type: text/html\n    Subject: Weather Notification\n    <h1>Test Subject</h1>\n    <p>\Test Body\</p>\n")
     end
+
+    it "sends the email" do
+      sender = double
+      allow(Net::SMTP).to receive(:start).and_return(sender)
+      allow(sender).to receive(:send_message)
+
+      expect(@mail.send_mail).to be_truthy
+    end
+
+    it "returns an error for localhost" do
+      expect(@mail.send_mail).to eq("Connection refused - connect(2) for \"localhost\" port 25")
+    end
   end
 end
--- spec/support/stubs.rb
+++ spec/support/stubs.rb
@@ -19,3 +19,14 @@ def forecast_stub
     }).
   to_return(status: 200, body: forecast.to_json, headers: {'Content-Type': 'application/json'})
 end
+
+def forecast_stub_missing_tuesday
+  stub_request(:get, "https://api.weather.gov/gridpoints/FWD/88,103/forecast").
+  with(
+    headers: {
+    'Accept'=>'*/*',
+    'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    'User-Agent'=>'Faraday v0.15.4'
+    }).
+  to_return(status: 200, body: forecast(tuesday: "nope").to_json, headers: {'Content-Type': 'application/json'})
+end
