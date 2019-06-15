529b1e3ceaa57e3f45d4cc976d07b87aac52d5d3
Josh Young <18503267+joshayoung@users.noreply.github.com>
2019-06-15 09:45:30 -0500

    Code cleanup

--- lib/day.rb
+++ lib/day.rb
@@ -4,13 +4,9 @@ class Day
   end
 
   def self.current(data)
-    @forecast ||= begin
-      forecast = []
-      data.each do |current_day|
-        forecast.push(new(current_day))
-      end
+    forecast = []
+    data.map { |day| forecast.push(new(day)) }
     forecast
-    end
   end
 
   def name
